4afc6b9cd6048fb156e4e0867c37cb31e7e5d9ff
ExplorerLeon <leon@explore.ai>
2024-05-15 09:41:55 +0200

    test create and wirte to gcs and bq

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 Before you start:
 https://cloud.google.com/bigquery/docs/cloud-storage-transfer#:~:text=Storage%20to%20BigQuery.-,Before%20you%20begin,BigQuery%20Data%20Transfer%20Service%2C%20see%20Specify%20encryption%20key%20with%20transfers.,-Limitations
 
+
+## To make Dataset
+Command required to create the dataset in big query. Will eventually be added to a bash script or cloud build yaml that will be executed from Cloud Build or bash command. TBD
+
 ```
 bq --location=europe-west2 mk \
 --dataset \
@@ -11,7 +15,5 @@ bq --location=europe-west2 mk \
 assetinsure-surety-data-models:asset_secure_test_dataset
 ```
 
-
-
-
+### Cloud Storage Bucket sample data URI
 gs://fs-assetsecure-testing/test_bg_df_sample_data.parquet
--- bq_tables_create.py
+++ bq_tables_create.py
@@ -1,18 +1,58 @@
+import sys
 
 from google.cloud import bigquery
 
-# Construct a BigQuery client object.
-client = bigquery.Client()
+def create_parquet_table(table_name, cloud_storage_file_uri, ):
+    """
+    Main Load function from parquet Google Cloud Storage file to
+    Big Query dataset with no tables initially.
+    """
 
-# TODO(developer): Set table_id to the ID of the model to fetch.
-# table_id = 'your-project.your_dataset.your_table'
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
 
-table = client.get_table(table_id)  # Make an API request.
+    # Set table_id to the ID of the table to create.
+    table_id = f"assetinsure-surety-data-models.asset_secure_test_dataset.{table_name}"
 
-# View table properties
-print(
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.PARQUET,
+    )
+
+    load_job = client.load_table_from_uri(
+        cloud_storage_file_uri, table_id, job_config=job_config
+    )  # Make an API request.
+
+    load_job.result()  # Waits for the job to complete.
+
+    destination_table = client.get_table(table_id)
+    print("Loaded {} rows.".format(destination_table.num_rows))
+
+
+def get_table_metadata(table_name):
+
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
+
+    # Set table_id to the ID of the model to fetch.
+    table_id = f"assetinsure-surety-data-models.asset_secure_test_dataset.{table_name}"
+    table = client.get_table(table_id)  # Make an API request.
+
+    # View table properties
+    print(
     "Got table '{}.{}.{}'.".format(table.project, table.dataset_id, table.table_id)
-)
-print("Table schema: {}".format(table.schema))
-print("Table description: {}".format(table.description))
-print("Table has {} rows".format(table.num_rows))
+    )
+    print("Table schema: {}".format(table.schema))
+    print("Table description: {}".format(table.description))
+    print("Table has {} rows".format(table.num_rows))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("usage: python bq_tables_create.py <table_name> <cloud_storage_file_uri>")
+        sys.exit(1)
+
+    table_name = sys.argv[1]
+    cloud_storage_file_uri = sys.argv[2]
+
+    create_parquet_table(table_name, cloud_storage_file_uri)
+
+    get_table_metadata(table_name)
